b85ef9d06e4002a633ec1fa4198d342f5b909980
Shubham <shubhamp489@gmail.com>
2024-06-20 20:51:12 +0530

    first commit

---
+++ SeleniumJavaBasic/Feature/Test1.feature
@@ -0,0 +1,6 @@
+@tag
+Feature: Launching WebBrowser
+   @tag1
+  Scenario: Launch web broswer
+    Given Open google chrome 
+  
---
+++ SeleniumJavaBasic/src/test/java/testRunner/TestRunner.java
@@ -0,0 +1,19 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "Feature",
+glue = "stepDefination",
+monochrome = true,plugin= {"pretty","html:target/cucumber-reports"},
+dryRun = false
+		)
+
+
+
+public class TestRunner {
+
+}
