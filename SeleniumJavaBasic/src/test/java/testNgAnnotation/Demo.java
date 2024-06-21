e35d66ef8297fdcd35f623c5b62e4400cb2e1bf8
Shubham <shubhamp489@gmail.com>
2024-06-21 22:18:05 +0530

    commiting test ng and cicd

---
+++ SeleniumJavaBasic/src/test/java/testNgAnnotation/Demo.java
@@ -0,0 +1,15 @@
+package testNgAnnotation;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void test() {
+		System.out.println("Hello testing the CI/CD process");
+	}
+	@Test
+	public void test2() {
+		System.out.println("Hello testing  the maven integration");
+	}
+}
